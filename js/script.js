0c57b89f0250361783e3a530ef0c6e39ccf5bde4
Letoy09 <136086251+Letoy09@users.noreply.github.com>
2023-06-22 07:09:35 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,4 +27,38 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+  $(document).ready(function(){
+    $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2500
+  });
+});
+window.addEventListener('DOMContentLoaded', function() {
+    var menuLinks = document.querySelectorAll('.menu a');
   
+    for (var i = 0; i < menuLinks.length; i++) {
+      menuLinks[i].addEventListener('click', scrollToSection);
+    }
+  
+    function scrollToSection(event) {
+      event.preventDefault();
+      var targetId = this.getAttribute('href');
+      var targetElement = document.querySelector(targetId);
+      var targetOffset = targetElement.offsetTop;
+  
+      window.scrollTo({
+        top: targetOffset,
+        behavior: 'smooth'
+      });
+    }
+  
+    window.addEventListener('scroll', function() {
+      var menu = document.querySelector('.menu');
+  
+      if (window.pageYOffset > 0) {
+        menu.classList.add('fixed-menu');
+      } else {
+        menu.classList.remove('fixed-menu');
+      }
+    });
+  });
